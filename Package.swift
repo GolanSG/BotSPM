993acb38ae1c63b6fac000286703278a4f41e84c
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2023-10-11 09:44:14 +0300

    Changed Core to Bot

--- Package.swift
+++ Package.swift
@@ -1,25 +1,27 @@
+// swift-tools-version: 5.4
 
-    // swift-tools-version:5.3
-    import PackageDescription
+import PackageDescription
 
-    let package = Package(
-        name: "Core",
-        platforms: [
-            .iOS(.v14)
-        ],
-        products: [
-            .library(
-                name: "Core",
-                targets: ["Core"]
-            ),
-        ],
-        dependencies: [.package(url: "https://github.com/GolanSG/CoreSPM.git", from: "1.5.2")],
-        targets: [
-            .binaryTarget(
-                name: "Core",
-                url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudBot/GenesysCloudBot_version_t1.5.0_commit_ddc5b1e32523dc63e132ebf4778063124162385a.zip",
-                checksum: "593f8f488313a545b88dbf899ea6dcd155a39b566658395730c3e3afaeb154bf"
-            ),
-        ]
-    )
+let package = Package(
+    name: "Bot",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "Bot",
+            targets: ["Bot"]
+        )
+    ],
     
+    dependencies: [.package(url: "https://github.com/GolanSG/CoreSPM.git",
+                            from: "1.5.2")],
+    targets: [
+        .binaryTarget(
+            name: "Bot",
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudBot/GenesysCloudBot_version_t1.5.0_commit_ddc5b1e32523dc63e132ebf4778063124162385a.zip",
+            checksum: "593f8f488313a545b88dbf899ea6dcd155a39b566658395730c3e3afaeb154bf"
+        )
+    ]
+)
+
